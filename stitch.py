e63d033a78ed4f79669e0862312a0d521c3a94d0
Jorge Zuluaga <jzuluaga@urania.udea.edu.co>
2018-01-14 22:59:43 -0500

    Commit

---
+++ stitch.py
@@ -0,0 +1,14 @@
+
+# coding: utf-8
+
+# # AIsteroid
+# [http://bit.ly/aisteroid](http://bit.ly/aisteroid)
+
+# In[1]:
+
+
+from aisteroid import *
+get_ipython().run_line_magic('matplotlib', 'nbagg')
+
+
+# ## Task: Template
